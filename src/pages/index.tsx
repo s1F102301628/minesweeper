ea6284d94a8d0338b3b4bb68e1ec3c66974a544e
s1F102301628 <s1F102301628@iniad.org>
2024-05-12 22:28:41 +0900

    いろいろ試し

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,19 @@ import styles from './index.module.css';
 
 const Home = () => {
   const [samplePos, setsamplePos] = useState(0);
-  console.log(samplePos);
-
+  const [board, setBoard] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  const clickHandler = (x: number, y: number) => {
+    if (board[y][x] !== 0) return;
+  };
   return (
     <div className={styles.container}>
       <div className={styles.baseStyle}>
@@ -15,13 +26,21 @@ const Home = () => {
         </div>
         <div className={styles.boardWrapper}>
           <div className={styles.boardStyle}>
-            <div className={styles.cellStyle}>
-              <div
-                className={styles.sampleStyle}
-                style={{ backgroundPosition: `${-30 * samplePos}px,0px` }}
-              />
-              <button onClick={() => setsamplePos((p) => (p + 1) % 14)}>sample</button>
-            </div>
+            {board.map((row, y) =>
+              row.map((color, x) => (
+                <div
+                  className={styles.cellStyle}
+                  key={`${x}-${y}`}
+                  onClick={() => clickHandler(x, y)}
+                >
+                  <div
+                    className={styles.sampleStyle}
+                    style={{ backgroundPosition: `${-30 * samplePos}px,0px` }}
+                  />
+                  <button onClick={() => setsamplePos((p) => (p + 1) % 14)}>sample</button>
+                </div>
+              )),
+            )}
           </div>
         </div>
       </div>
