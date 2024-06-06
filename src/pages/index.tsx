0dedec47cd5e6b31f820567deca9ec2d9dd8ecf1
s1F102301628 <s1f102301628@iniad.org>
2024-06-07 02:16:55 +0900

    参照しながら作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,15 +4,15 @@ import styles from './index.module.css';
 const Home = () => {
   // 指定された初期ボード
   const Board = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
   ];
   const [BombBoard, setBombBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -25,7 +25,7 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
-  const [UserBoard, setUserBoard] = useState([
+  const [UserBoard, setUserBoard] = useState<(0 | 1 | 2 | 3)[][]>([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -36,40 +36,95 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
-  const [isFirstClick, setIsFirstClick] = useState(true);
-
-  const BombCreate = (board: number[][], x: number, y: number): number[][] => {
+  const directions = [
+    [-1, 0],
+    [-1, -1],
+    [0, -1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+    [0, 1],
+    [-1, 1],
+  ];
+  // -1 = 石
+  // 0 = 画像なしセル
+  // 1~8 画像セル
+  // 9 石＋はてな
+  // 10 石＋旗
+  // 11 bomb
+  const newBombBoard: number[][] = JSON.parse(JSON.stringify(BombBoard));
+  const newUserBoard: (0 | 1 | 2 | 3)[][] = JSON.parse(JSON.stringify(UserBoard));
+  const isPlaying = UserBoard.some((row) => row.some((input) => input !== 0));
+  const isFailure = UserBoard.some((row, y) =>
+    row.some((input, x) => input === 1 && BombBoard[y][x] === 1),
+  );
+  const BombCreate = (x: number, y: number) => {
     let bombCount = 0;
     const newBombBoard = structuredClone(BombBoard);
     while (bombCount < 10) {
       const bombY = Math.floor(Math.random() * 9);
       const bombX = Math.floor(Math.random() * 9);
-      if (newBombBoard[bombY][bombX] === 1 || (bombY === y && bombX === x)) continue;
-      newBombBoard[bombY][bombX] = 1;
-      bombCount++;
+      if (newBombBoard[bombY][bombX] !== 1 && (bombY !== y || bombX !== x)) {
+        newBombBoard[bombY][bombX] = 1;
+        bombCount++;
+      }
     }
-    return newBombBoard;
   };
 
-  const clickHandler = (x: number, y: number) => {
-    if (BombBoard[y][x] === 1) {
-      return; // 爆弾の場合は何もしない
+  const checkAround = (x: number, y: number) => {
+    let AroundBombCount = 0;
+    for (const dir of directions) {
+      if (Board[y + dir[1]] !== undefined && Board[y + dir[1]][x + dir[0]] !== undefined) {
+        AroundBombCount += BombBoard[y + dir[1]][x + dir[0]];
+      }
     }
-    if (isFirstClick) {
-      const initialBombBoard = BombCreate(Board, x, y);
-      setBombBoard(initialBombBoard);
-      setIsFirstClick(false);
+    Board[y][x] = AroundBombCount;
+    if (AroundBombCount === 0) {
+      for (const dir of directions) {
+        if (Board[y + dir[1]] !== undefined && Board[y + dir[1]][x + dir[0]] === -1) {
+          checkAround(x + dir[0], y + dir[1]);
+        }
+      }
     }
-    // const newBoard = structuredClone(Board);
-    // newBoard[y][x] = BombBoard[y][x] === 1 ? -1 : 1; // 例として、爆弾なら -1、爆弾でなければ 1 に設定
-    // setBoard(newBoard);
-
-    // else {
-    //   // 2回目以降のクリック
-    //   // ここで適切な処理を実装する
-    // }
   };
-
+  for (let y = 0; y < 9; y++) {
+    for (let x = 0; x < 9; x++) {
+      if (UserBoard[y][x] === 1) {
+        if (BombBoard[y][x] === 1) {
+          Board[y][x] = 11;
+        } else {
+          checkAround(x, y);
+        }
+      }
+    }
+  }
+  //爆弾をクリックしたら全部表示させる
+  if (isFailure) {
+    for (let y = 0; y < 9; y++) {
+      for (let x = 0; x < 9; x++) {
+        if (BombBoard[y][x] === 1) {
+          Board[y][x] = 11;
+        }
+      }
+    }
+  }
+  const onClick = (x: number, y: number) => {
+    newUserBoard[y][x] = 1;
+    setUserBoard(newUserBoard);
+    if (!isPlaying) {
+      BombCreate(x, y);
+    }
+    let BombExist = false;
+    for (const row of BombBoard) {
+      for (const cell of row) {
+        if (cell === 1) {
+          BombExist = true;
+          break;
+        }
+      }
+    }
+    setBombBoard(newBombBoard);
+  };
   return (
     <div className={styles.container}>
       <div className={styles.baseStyle}>
@@ -78,19 +133,19 @@ const Home = () => {
           <div className={styles.scoreboardStyle} />
           <div className={styles.timeBoardStyle} />
         </div>
-        <div className={styles.boardWrapper}>
-          <div className={styles.boardStyle}>
-            {Board.map((row, y) =>
-              row.map((cell, x) => (
-                <div
-                  className={styles.bomb}
-                  key={`${y}-${x}`}
-                  onClick={() => clickHandler(x, y)}
-                  style={{ backgroundPosition: cell * -30 + 30 }}
-                />
-              )),
-            )}
-          </div>
+        <div className={styles.board}>
+          {Board.map((row, y) =>
+            row.map((color, x) => (
+              <div
+                className={styles.bomb}
+                key={`${x}-${y}`}
+                onClick={() => onClick(x, y)}
+                style={{ backgroundPosition: color * -30 + 30 }}
+              >
+                {color === -1 && <div className={styles.stone} />}
+              </div>
+            )),
+          )}
         </div>
       </div>
     </div>
